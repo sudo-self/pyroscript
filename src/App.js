2d50d779dae1a1602c021c5174495a9b6b078f74
Sudo-Self <sudo-self@proton.me>
2024-03-31 05:05:11 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ export default function App() {
           setInputValue('');
           confetti();
         } else {
-          alert('Hello! Thanks for using pyroscript messenger. You must be signed to post messages!');
+          alert('Thank you for using Pyroscript Messenger. You must be signed to post messages.');
         }
       } catch (error) {
         console.error('Error submitting form:', error);
@@ -144,15 +144,15 @@ export default function App() {
           </div>
           <form id="guestbookForm" className="flex items-center mt-2 relative mx-auto max-w-md" onSubmit={handleSubmit}>
           <input
-  type="text"
-  id="entry"
-  name="entry"
-  placeholder="say something nice.."
-  value={inputValue}
-  onChange={(e) => setInputValue(e.target.value)}
-  className="border-gray-300 rounded-md py-2 px-4 mr-2 focus:outline-none focus:border-green-900 flex-grow input-dark-mode"
-  style={{ width: '100%', color: 'black' }} // Set color to black
-/>
+            type="text"
+            id="entry"
+            name="entry"
+            placeholder="say something nice.."
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            className="border-gray-300 rounded-md py-2 px-4 mr-2 focus:outline-none focus:border-green-900 flex-grow input-dark-mode"
+            style={{ width: '100%', color: 'black' }}
+          />
             <button type="submit" className="bg-gray-900 text-white px-4 py-2 rounded-md transition duration-300 hover:bg-pyro focus:outline-none focus:bg-gray-900 confetti-btn">
               send
             </button>
